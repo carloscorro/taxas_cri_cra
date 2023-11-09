2a22f9c6d2910eb9b679264b9252cd0599d76462
Carlos Corro <c.e.mercedescorro@hotmail.com>
2023-11-09 16:29:32 -0300

    Criação do Dataframe

--- webscrapping_anbima_data.py
+++ webscrapping_anbima_data.py
@@ -9,4 +9,28 @@ soup = BeautifulSoup(r.text, "lxml")
 
 table = soup.find("table", class_ = "custom-anbi-ui-table")
 
-print(table)
+headers = table.find_all("th")
+
+# print(headers)
+
+titles = []
+
+for i in headers:
+    title = i.text
+    titles.append(title)
+
+df = pd.DataFrame(columns=titles)
+
+rows = table.find_all("tr")
+
+for i in rows[1:]:
+    data = i.find_all("td")
+    row = [tr.text for tr in data]
+    l = len(df)
+    df.loc[l] = row
+
+print(df)
+
+
+
+# print(titles)
